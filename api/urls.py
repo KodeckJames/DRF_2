2ace0467a5e8fb639f9305b8dbc50289763b792f
Kodeck James <kodeckjames095@gmail.com>
2024-07-18 17:22:28 +0300

    Second commit

---
+++ api/urls.py
@@ -0,0 +1,6 @@
+from home.views import index
+from django.urls import path
+
+urlpatterns = [
+    path('index/', index)
+]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+
+@api_view(['GET', 'POST','PUT'])
+def index(request):
+    courses = {
+        "course_name":"Python",
+        "learn": ['flask', 'Django', 'Tornado', 'FastApi'],
+        "course_provider":'Scalers'
+    }
+    if request.method == 'GET':
+        print('You hit a GET request')
+        return Response(courses)
+    elif request.method == 'POST':
+        print('You hit a POST request')
+        return Response(courses)
+    elif request.method == 'PUT':
+        print('You hit a PUT request')
+        return Response(courses)
