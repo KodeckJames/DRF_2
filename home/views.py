15037ff198de4654c2b9bf0509f0dd204a6561fa
Kodeck James <kodeckjames095@gmail.com>
2024-07-19 12:07:25 +0300

    second commit

--- home/views.py
+++ home/views.py
@@ -1,5 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from home.models import Person
+from home.serializers import PeopleSerializer
+
 # Create your views here.
 
 @api_view(['GET', 'POST','PUT'])
@@ -22,3 +26,17 @@ def index(request):
     elif request.method == 'PUT':
         print('You hit a PUT request')
         return Response(courses)
+    
+@api_view(['GET','POST'])
+def person(request):
+    if request.Get:
+        objs = person.objects.all()
+        serializer = PeopleSerializer(objs, many = True)
+        return Response(serializer.data)
+    else:
+        data = request.data
+        serializer = PeopleSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
