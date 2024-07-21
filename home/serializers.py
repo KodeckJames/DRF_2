289f9056fb2f436769249aa9a94b481a25376503
Kodeck James <kodeckjames095@gmail.com>
2024-07-21 19:58:37 +0300

    Fifth commit

--- home/serializers.py
+++ home/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import Person
 
-# Write your classes here:
 class PeopleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
