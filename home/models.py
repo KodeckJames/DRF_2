fb22fd34a1789ebd8b2736156b4c23429178ec4a
Kodeck James <kodeckjames095@gmail.com>
2024-07-22 13:04:18 +0300

    Sixth commit

--- home/models.py
+++ home/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -27,13 +27,27 @@ def index(request):
         print('You hit a PUT request')
         return Response(courses)
     
-@api_view(['GET','POST'])
+@api_view(['GET','POST','PUT','PATCH'])
 def person(request):
     if request.method == 'GET':
-        objs = person.objects.all()
+        objs = Person.objects.all()
         serializer = PeopleSerializer(objs, many = True)
         return Response(serializer.data)
-    else:
+    elif request.method == 'POST':
+        data = request.data
+        serializer = PeopleSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    elif request.method == 'PUT':
+        data = request.data
+        serializer = PeopleSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    elif request.method == 'PATCH':
         data = request.data
         serializer = PeopleSerializer(data = data)
         if serializer.is_valid():
